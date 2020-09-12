7ca4820cc00e0725a589439c24b292a6a2686c1d
Andrea Guffi <andreaguffi92@gmail.com>
2020-09-12 15:34:56 +0200

    prova prova

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'DeliMeals',
+      title: 'DeliMealss',
       theme: ThemeData(
         primarySwatch: Colors.pink,
         accentColor: Colors.amber,
