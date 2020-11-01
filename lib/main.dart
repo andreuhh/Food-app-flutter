6315868a68555e26cbb9cea6b821689a95e92cb0
Andrea Guffi <andreaguffi92@gmail.com>
2020-11-01 10:21:05 +0100

    continued with categories & favorites

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
-import 'package:appcibo/screens/meal_detail_screen.dart';
 import 'package:flutter/material.dart';
+
+import './screens/tabs_screen.dart';
+import './screens/meal_detail_screen.dart';
 
 import 'screens/category_meals_screen.dart';
 import 'screens/categories_screen.dart';
@@ -32,7 +34,7 @@ class MyApp extends StatelessWidget {
       //home: CategoriesScreen(),
       initialRoute: '/', // default is '/'
       routes: {
-        '/': (ctx) => CategoriesScreen(),
+        '/': (ctx) => TabsScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
       },
