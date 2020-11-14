ae53b7ec7fb18b1a75514e694d7ea89566c3ad2e
Andrea Guffi <andreaguffi92@gmail.com>
2020-11-14 22:01:59 +0100

    removing element ok

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../widgets/main_drawer.dart';
 
 class FiltersScreen extends StatelessWidget {
   static const routeName = '/filters';
@@ -9,6 +10,7 @@ class FiltersScreen extends StatelessWidget {
       body: Center(
         child: Text('Filters!'),
       ),
+      drawer: MainDrawer(),
     );
   }
 }
